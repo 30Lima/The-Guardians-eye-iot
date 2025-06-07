2e0721d2d10b7c583c7630b2bd8128a8f8362fe3
30lima <pedrolimaa354@gmail.com>
2025-06-07 16:09:14 -0300

    feat: configure serial communication, GPS, and LED pin

--- esp32/main.ino
+++ esp32/main.ino
@@ -7,3 +7,12 @@
 #define AIR_QUALITY_PIN 35
 #define LED_PIN 2
 
+DHT dht(DHTPIN, DHTTYPE);
+TinyGPSPlus gps;
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, 16, 17); // GPS ligado RX=16, TX=17 no ESP32
+  dht.begin();
+  pinMode(LED_PIN, OUTPUT);
+}
