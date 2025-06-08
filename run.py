62f3f78eac6ed8ff37d5a23de6dd05ca9c6d0f70
30lima <pedrolimaa354@gmail.com>
2025-06-08 06:33:33 -0300

    feat: add new launcher

--- run.py
+++ run.py
@@ -1,35 +1,6 @@
-from flask import Flask, render_template, request, jsonify
-import json
-import os
-from datetime import datetime
+from app import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-DATA_FILE = 'data/log.json'
-
-# Garante que o arquivo exista
-if not os.path.exists(DATA_FILE):
-    with open(DATA_FILE, 'w') as f:
-        json.dump([], f)
-
-@app.route('/')
-def index():
-    with open(DATA_FILE, 'r') as f:
-        data = json.load(f)
-    return render_template('index.html', data=data)
-
-@app.route('/api/data', methods=['POST'])
-def receive_data():
-    content = request.json
-    content['timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    with open(DATA_FILE, 'r+') as f:
-        log = json.load(f)
-        log.insert(0, content)
-        f.seek(0)
-        json.dump(log, f, indent=2)
-
-    return jsonify({"status": "ok"})
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
